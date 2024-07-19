b7cbe790230c83de3095e1fa23132bc77431d1b3
Ivy <ivyjkemboi@gmail.com>
2024-07-19 22:25:00 +0300

    first

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,104 @@
 class Article:
+    all_articles = [] 
+
     def __init__(self, author, magazine, title):
-        self.author = author
-        self.magazine = magazine
-        self.title = title
-        
+        self._author = author
+        self._magazine = magazine
+        self._title = title
+        Article.all_articles.append(self)
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        raise AttributeError("Title is immutable")
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, value):
+        self._author = value
+
+    @property
+    def magazine(self):
+        return self._magazine
+
+    @magazine.setter
+    def magazine(self, value):
+        self._magazine = value
+
+
 class Author:
     def __init__(self, name):
-        self.name = name
+        self._name = name
+        self._articles = []
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        raise AttributeError("Name is immutable")
 
     def articles(self):
-        pass
+        return [article for article in Article.all_articles if article.author == self]
 
     def magazines(self):
-        pass
+        return list(set(article.magazine for article in self.articles()))
 
     def add_article(self, magazine, title):
-        pass
+        new_article = Article(self, magazine, title)
+        self._articles.append(new_article)
+        return new_article
 
     def topic_areas(self):
-        pass
+        return list(set(magazine.category for magazine in self.magazines()))
+
 
 class Magazine:
     def __init__(self, name, category):
-        self.name = name
-        self.category = category
+        self._name = name
+        self._category = category
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str) or not (2 <= len(value) <= 16):
+            raise ValueError("Name must be a string between 2 and 16 characters inclusive")
+        self._name = value
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, value):
+        if not isinstance(value, str) or len(value) == 0:
+            raise ValueError("Category must be a non-empty string")
+        self._category = value
 
     def articles(self):
-        pass
+        return [article for article in Article.all_articles if article.magazine == self]
 
     def contributors(self):
-        pass
+        return list(set(article.author for article in self.articles()))
 
     def article_titles(self):
-        pass
+        return [article.title for article in self.articles()]
 
     def contributing_authors(self):
-        pass
+        author_count = {}
+        for article in self.articles():
+            if article.author in author_count:
+                author_count[article.author] += 1
+            else:
+                author_count[article.author] = 1
+        return [author for author, count in author_count.items() if count > 2]
